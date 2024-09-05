27c91d02b695a2988bee44511f549547e6468ea8
Dave Porter <dave@dj-software.com.au>
2024-09-05 16:30:41 +0800

    before first deploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Safe-T-Products Calculator</h1>
+<p>Intro text!</p>
