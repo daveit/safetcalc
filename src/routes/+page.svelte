ee2a7428121a84892bc87dbecc7f092c578b4ac6
Dave Porter <dave@dj-software.com.au>
2024-09-06 14:40:08 +0800

    installed Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to Safe-T-Products Calculator</h1>
-<p>Intro text!</p>
+<h1 class="text-3xl font-bold underline text-amber-900">
+	Hello world!
+</h1>
+<p class="border-gray-500 accent-green-700">Hi there with a border</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
